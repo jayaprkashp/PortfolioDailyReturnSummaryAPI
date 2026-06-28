fc2376cdea24467e7b51d18847cef5abe5293e78
jayaprkashp <jayaprakashp_777@outlook.com>
2026-06-29 00:22:11 +0530

    updated with welcome print message

--- src/main/java/com/portfolio/dailyreturn/summaryapi/SummaryapiApplication.java
+++ src/main/java/com/portfolio/dailyreturn/summaryapi/SummaryapiApplication.java
@@ -8,6 +8,7 @@ public class SummaryapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SummaryapiApplication.class, args);
+		System.out.println("Welcome to Portfolio Daily Return Summary App");
 	}
 
 }
