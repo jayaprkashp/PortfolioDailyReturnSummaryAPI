14f26c5f05747bdfc696272ce8c73cf0a00b5d71
jayaprkashp <jayaprakashp_777@outlook.com>
2026-06-28 16:43:06 +0530

    Initial project commit

---
+++ src/main/java/com/portfolio/dailyreturn/summaryapi/SummaryapiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dailyreturn.summaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/dailyreturn/summaryapi/SummaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dailyreturn.summaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
